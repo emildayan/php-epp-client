c582378cabc19a31725411d6e8185afdf204e1ea
emildayan <emil@emildayan.se>
2021-11-03 10:43:15 +0100

    Update iisEppInfoDomainResponse.php

--- Protocols/EPP/eppExtensions/iis-1.2/eppResponses/iisEppInfoDomainResponse.php
+++ Protocols/EPP/eppExtensions/iis-1.2/eppResponses/iisEppInfoDomainResponse.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Metaregistrar\EPP;
 
 /*
@@ -10,8 +11,10 @@ namespace Metaregistrar\EPP;
 </extension>
  */
 
-class iisEppInfoDomainResponse extends eppInfoDomainResponse {
-    function __construct() {
+class iisEppInfoDomainResponse extends eppInfoDomainResponse
+{
+    function __construct()
+    {
         parent::__construct();
     }
 
@@ -20,12 +23,16 @@ class iisEppInfoDomainResponse extends eppInfoDomainResponse {
      *
      * @return string State
      */
-    public function getDomainState() {
+    public function getDomainState()
+    {
         $xpath = $this->xPath();
         $result = $xpath->query('/epp:epp/epp:response/epp:extension/iis:infData/iis:state');
-        if ($result->length > 0) {
+        if ($result->length > 0)
+        {
             return $result->item(0)->nodeValue;
-        } else {
+        }
+        else
+        {
             return null;
         }
     }
@@ -35,12 +42,16 @@ class iisEppInfoDomainResponse extends eppInfoDomainResponse {
      *
      * @return 0 or 1
      */
-    public function getDomainClientDelete() {
+    public function getDomainClientDelete()
+    {
         $xpath = $this->xPath();
         $result = $xpath->query('/epp:epp/epp:response/epp:extension/iis:infData/iis:clientDelete');
-        if ($result->length > 0) {
+        if ($result->length > 0)
+        {
             return $result->item(0)->nodeValue;
-        } else {
+        }
+        else
+        {
             return null;
         }
     }
@@ -49,12 +60,16 @@ class iisEppInfoDomainResponse extends eppInfoDomainResponse {
      *
      * @return String
      */
-    public function getDomainDeactDate() {
+    public function getDomainDeactDate()
+    {
         $xpath = $this->xPath();
         $result = $xpath->query('/epp:epp/epp:response/epp:extension/iis:infData/iis:deactDate');
-        if ($result->length > 0) {
+        if ($result->length > 0)
+        {
             return $result->item(0)->nodeValue;
-        } else {
+        }
+        else
+        {
             return null;
         }
     }
@@ -64,14 +79,49 @@ class iisEppInfoDomainResponse extends eppInfoDomainResponse {
      *
      * @return String
      */
-    public function getDomainDelDate() {
+    public function getDomainDelDate()
+    {
         $xpath = $this->xPath();
         $result = $xpath->query('/epp:epp/epp:response/epp:extension/iis:infData/iis:delDate');
-        if ($result->length > 0) {
+        if ($result->length > 0)
+        {
             return $result->item(0)->nodeValue;
-        } else {
+        }
+        else
+        {
             return null;
         }
     }
-}
 
+
+    public function getKeydata()
+    {
+        // Check if dnssec is enabled on this interface
+        if ($this->findNamespace('secDNS'))
+        {
+            $xpath = $this->xPath();
+            $result = $xpath->query('/epp:epp/epp:response/epp:extension/secDNS:infData/*');
+            $keys = array();
+            if (count($result) > 0)
+            {
+                foreach ($result as $keydata)
+                {
+                    /* @var $keydata \DOMElement */
+                    // Check if the keyTag element is present. If not, use getKeys()
+                    $test = $keydata->getElementsByTagName('keyTag');
+                    if ($test->length > 0)
+                    {
+                        $secdns = new eppSecdns();
+                        $secdns->setKeytag($keydata->getElementsByTagName('keyTag')->item(0)->nodeValue);
+                        $secdns->setAlgorithm($keydata->getElementsByTagName('alg')->item(0)->nodeValue);
+                        $secdns->setDigestType($keydata->getElementsByTagName('digestType')->item(0)->nodeValue);
+                        $secdns->setDigest($keydata->getElementsByTagName('digest')->item(0)->nodeValue);
+                        $keys[] = $secdns;
+                    }
+                }
+            }
+            return $keys;
+        }
+        return null;
+    }
+}
